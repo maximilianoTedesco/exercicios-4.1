b47b0d3dcb832f69ab2f75880decef73aff3ef21
maximilianoTedesco <maxytedesco@gmail.com>
2022-07-12 14:41:32 -0300

    switch

---
+++ Exercicio Switch/main.js
@@ -0,0 +1,97 @@
+/*const h1 = document.querySelector('.container h1');
+const data = new Date();
+
+function getDiaSemanaTexto(diaSemana) {
+    let diaSemanaTexto;
+
+    switch (diaSemana) {
+        case 0:
+            diaSemanaTexto = 'Domingo';
+            return diaSemanaTexto;
+        case 1:
+            diaSemanaTexto = 'Segunda-feira';
+            return diaSemanaTexto;
+        case 2:
+            diaSemanaTexto = 'Terça-feira';
+            return diaSemanaTexto;
+        case 3:
+            diaSemanaTexto = 'Quarta-feira';
+            return diaSemanaTexto;
+        case 4:
+            diaSemanaTexto = 'Quinta-feira';
+            return diaSemanaTexto;
+        case 5:
+            diaSemanaTexto = 'Sexta-feira';
+            return diaSemanaTexto;
+        case 6:
+            diaSemanaTexto = 'Sabado';
+            return diaSemanaTexto;
+        default:
+            diaSemanaTexto = 'Ta errado'
+            return diaSemanaTexto;
+
+    }
+}
+
+function zeroAEsquerda(num) {
+    return num >= 10 ? num : `0${num}`
+}
+
+function getNomeMes(numeroMes) {
+    let mesTexto;
+
+    switch (numeroMes) {
+        case 0:
+            mesTexto = 'Janeiro';
+            return mesTexto;
+        case 1:
+            mesTexto = 'Fevereiro';
+            return mesTexto;
+        case 2:
+            mesTexto = 'Março';
+            return mesTexto;
+        case 3:
+            mesTexto = 'Abril';
+            return mesTexto;
+        case 4:
+            mesTexto = 'Maio';
+            return mesTexto;
+        case 5:
+            mesTexto = 'Junho';
+            return mesTexto;
+        case 6:
+            mesTexto = 'Julho';
+            return mesTexto;
+        case 7:
+            mesTexto = 'Agosto';
+            return mesTexto;
+        case 8:
+            mesTexto = 'Setembro';
+            return mesTexto;
+        case 9:
+            mesTexto = 'Outubro';
+            return mesTexto;
+        case 10:
+            mesTexto = 'Novembro';
+            return mesTexto;
+        case 11:
+            mesTexto = 'Dezembro';
+            return mesTexto;
+    }
+}
+function criaData(data) {
+    const diaSemana = data.getDay();
+    const numeroMes = data.getMonth();
+
+    const nomeDia = getDiaSemanaTexto(diaSemana)
+    const nomeMes = getNomeMes(numeroMes)
+
+    return (`${nomeDia}, ${data.getDate()} de ${nomeMes}` +
+        ` de ${data.getFullYear()}, ` +
+        `${zeroAEsquerda(data.getHours())}:${zeroAEsquerda(data.getMinutes())}`)
+ }
+h1.innerHTML = criaData(data)*/
+
+const h1 = document.querySelector('.container h1')
+const valor = new Date()
+h1.innerHTML = valor.toLocaleDateString('pt-BR', { dateStyle: 'full'})
---
+++ Switch/Case/index.js
@@ -0,0 +1,31 @@
+const data = new Date('1992-02-25 00:00:00')
+const diaSemana = data.getDay()
+let diaSemanaTexto;
+
+switch (diaSemana) {
+    case 0:
+        diaSemanaTexto = 'Domingo';
+        break;
+    case 1:
+        diaSemanaTexto = 'Segunda';
+        break;
+    case 2:
+        diaSemanaTexto = 'Terça';
+        break;
+    case 3:
+        diaSemanaTexto = 'Quarta';
+        break;
+    case 4:
+        diaSemanaTexto = 'Quinta';
+        break;
+    case 5:
+        diaSemanaTexto = 'Sexta';
+        break;
+    case 6:
+        diaSemanaTexto = 'Sabado';
+        break;
+    default:
+        diaSemanaTexto = 'Ta errado'
+}
+
+console.log(diaSemana, diaSemanaTexto)
